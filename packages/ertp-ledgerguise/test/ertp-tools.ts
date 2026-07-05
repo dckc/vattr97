287d57f184483b98f46ef747cfa9d145dc74b7a2
anilhelvaci <anilhelvaci96@gmail.com>
2026-07-05 14:57:34 +0300

    fix: style fix
    #1

--- packages/ertp-ledgerguise/test/ertp-tools.ts
+++ packages/ertp-ledgerguise/test/ertp-tools.ts
@@ -8,27 +8,29 @@ import type {
 type Dollars = `$${string}`;
 const numeral = (amt: Dollars) => amt.replace(/[$,]/g, '');
 
-export const withAmountUtils = (kit: IssuerKit<'nat'>) => Object.freeze({
-  ...kit,
-  amount: (value: bigint): NatAmount => ({ brand: kit.brand, value }),
-  $: (amt: Dollars): NatAmount => ({
-    brand: kit.brand,
-    value: BigInt(numeral(amt)),
-  }),
-  fund: (purse: Purse<'nat'>, value: bigint) =>
-    purse.deposit(kit.mint.mintPayment({ brand: kit.brand, value })),
-  fundDeposit: (deposit: DepositFacet<'nat'>, value: bigint) =>
-    deposit.receive(kit.mint.mintPayment({ brand: kit.brand, value })),
-});
+export const withAmountUtils = (kit: IssuerKit<'nat'>) =>
+  Object.freeze({
+    ...kit,
+    amount: (value: bigint): NatAmount => ({ brand: kit.brand, value }),
+    $: (amt: Dollars): NatAmount => ({
+      brand: kit.brand,
+      value: BigInt(numeral(amt)),
+    }),
+    fund: (purse: Purse<'nat'>, value: bigint) =>
+      purse.deposit(kit.mint.mintPayment({ brand: kit.brand, value })),
+    fundDeposit: (deposit: DepositFacet<'nat'>, value: bigint) =>
+      deposit.receive(kit.mint.mintPayment({ brand: kit.brand, value })),
+  });
 
-export const ertpOnly = <T extends IssuerKit<'nat'>>(kit: T) => Object.freeze({
-  issuer: kit.issuer,
-  brand: kit.brand,
-  amount: (value: bigint): NatAmount => ({ brand: kit.brand, value }),
-  $: (amt: Dollars): NatAmount => ({
+export const ertpOnly = <T extends IssuerKit<'nat'>>(kit: T) =>
+  Object.freeze({
+    issuer: kit.issuer,
     brand: kit.brand,
-    value: BigInt(numeral(amt)),
-  }),
-  mintRecoveryPurse: kit.mintRecoveryPurse,
-  displayInfo: kit.displayInfo,
-});
+    amount: (value: bigint): NatAmount => ({ brand: kit.brand, value }),
+    $: (amt: Dollars): NatAmount => ({
+      brand: kit.brand,
+      value: BigInt(numeral(amt)),
+    }),
+    mintRecoveryPurse: kit.mintRecoveryPurse,
+    displayInfo: kit.displayInfo,
+  });
